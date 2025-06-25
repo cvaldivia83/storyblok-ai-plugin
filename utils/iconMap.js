aabe69815f3f39ed62acb1903cf483d808edc13c
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-25 01:13:47 -0300

    Add icon list with all icons available in this app

---
+++ utils/iconMap.js
@@ -0,0 +1,15 @@
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import DoneIcon from '@mui/icons-material/Done';
+import MonetizationOn from '@mui/icons-material/MonetizationOn';
+import SpeedIcon from '@mui/icons-material/Speed';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+
+export const iconMap = {
+  'trending_up': TrendingUpIcon,
+  'access_time': AccessTimeIcon,
+  'monetization_on': MonetizationOn,
+  'campaign': CampaignIcon,
+  'speed': SpeedIcon,
+  'done': DoneIcon,
+}
