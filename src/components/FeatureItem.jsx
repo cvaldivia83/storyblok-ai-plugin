bd7a61e61d3956ea70a6f1960099742ac96f0f2b
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-25 18:36:32 -0300

    Add styles to feature item

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,11 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
+import { iconMap } from '../../utils/iconMap';
+import { useMediaQuery } from '@mui/material';
 
 const FeatureItem = ({ blok }) => {
+
+  const matches = useMediaQuery('(max-width:700px)');
+
+  const IconComponent = iconMap[blok.icon] || iconMap.done;
+
   return (
-    <Box sx={{ mb: 3 }} >
-      <Typography variant="h6" component="h3">{blok.title}</Typography>
-      <Typography variant="body2" color="text-secondary">{blok.description}</Typography>
-    </Box>
+   <Stack height={matches ? "366px" : "300px"} direction="column" spacing={2} alignItems="center" justifyContent="center" padding={4} border={1} borderRadius={2} borderColor="rgb(200, 200, 200)">
+      <Box>
+        <IconComponent />
+      </Box>
+
+      <Box sx={{ mb: 3 }} >
+        <Typography variant="h6" component="h3" textAlign="center" mb={2}>{blok.title}</Typography>
+        <Typography variant="body1" color="text-secondary" textAlign="center">{blok.description}</Typography>
+      </Box>
+   </Stack>
   )
 }
 
