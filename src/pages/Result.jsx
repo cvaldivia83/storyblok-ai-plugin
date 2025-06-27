f282165926eecab2338990dc86ad456ba190bd76
Carla Valdivia <valdivia.carla@gmail.com>
2025-06-26 23:29:13 -0300

    Add page result

---
+++ src/pages/Result.jsx
@@ -0,0 +1,11 @@
+const Result = () => {
+  return (
+    <div>
+      <h1>Boom! Your content is ready.</h1>
+      <p>We brewed up some killer copy, fresh keywords, and alt text magic just for you.</p>
+      <small>Want to tweak something? You can always go back and edit.</small>
+    </div>
+  )
+}
+
+export default Result;
